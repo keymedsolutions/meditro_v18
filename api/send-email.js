0598c81c1580411bc5eaa155fb9aee5ba517acde
Keymedsolution <admin@keymedsolution.com>
2025-04-17 20:10:28 +0530

    -->Fix cont form

--- api/send-email.js
+++ api/send-email.js
@@ -1,47 +1,77 @@
 const nodemailer = require("nodemailer");
 
-
 export default async function handler(req, res) {
-  const SmtpEmail = process.env.NEXT_EMAIL_ADDRESS
-  const SmtpPassword = process.env.NEXT_EMAIL_PASSWORD
-  if(req.method === "POST") {
-      const transporter = nodemailer.createTransport({
-        host: "smtp.hostinger.com", // Hostinger SMTP server
-        port: 587, // Use 465 for SSL, 587 for TLS
-        secure: false, // true for SSL, false for TLS
-        auth: {
-          user:SmtpEmail,
-          pass: SmtpPassword, // Your Hostinger email password
-        },
-        tls: {
-          rejectUnauthorized: false, // Prevents SSL issues
-        },
+  const SmtpEmail = process.env.NEXT_EMAIL_ADDRESS;
+  const SmtpPassword = process.env.NEXT_EMAIL_PASSWORD;
+  if (req.method === "POST") {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.hostinger.com", // Hostinger SMTP server
+      port: 587, // Use 465 for SSL, 587 for TLS
+      secure: false, // true for SSL, false for TLS
+      auth: {
+        user: SmtpEmail,
+        pass: SmtpPassword, // Your Hostinger email password
+      },
+      tls: {
+        rejectUnauthorized: false, // Prevents SSL issues
+      },
+    });
+
+    let subject = "New Evaluation Form Submission";
+   
+    let html = "";
+    if (req.body.usedFor === "contactus") {
+      subject = "New Contact Form Submission";
+      const details = [];
+      delete req.body.usedFor;
+      Object.keys(req.body).forEach((key) => {
+        const html = `<p style="margin: 10px 0;"><strong>${key}:</strong>${req.body[key]}</p>`;
+        details.push(html);
       });
 
+      html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>New Contact Form Submission</title>
+</head>
+<body style="font-family: Arial, sans-serif; color: #333; line-height: 1.6; margin: 0; padding: 0;">
 
+    <div style="width: 100%; max-width: 600px; margin: 0 auto; padding: 20px; background-color: #f4f4f4;">
+        <div style="background-color: #007BFF; color: white; padding: 10px; text-align: center; border-radius: 8px;">
+            <h2 style="margin: 0; font-size: 24px;">New Contact Form Submission</h2>
+        </div>
+
+        <div style="background-color: #ffffff; padding: 20px; border-radius: 8px; margin-top: 20px; border: 1px solid #ddd;">
+    ${details.join("")}
+            <p style="margin: 10px 0;"><strong>Submitted on:</strong>${new Date().toLocaleString()}</p>
+        </div>
+
+    </div>
+
+</body>
+</html>`;
+    } else {
       const { name, email, phone, practice } = req.body;
-
-      const mailOptions = {
-        from: `"KeyMed Solution" <${SmtpEmail}>`, // Sender name & email
-        to: SmtpEmail, // Recipient email
-        subject: "New Evaluation Form Submission",
-        html: `
-          <h1>New Evaluation Form Submission</h1>
-        <p><strong>Name:</strong> ${name}</p>
-        <p><strong>Email:</strong> ${email}</p>
-        <p><strong>Phone:</strong> ${phone}</p>
-        <p><strong>Practice Name:</strong> ${practice}</p>
-      `,
+      html = `
+      <h1>New Evaluation Form Submission</h1>
+    <p><strong>Name:</strong> ${name}</p>
+    <p><strong>Email:</strong> ${email}</p>
+    <p><strong>Phone:</strong> ${phone}</p>
+    <p><strong>Practice Name:</strong> ${practice}</p>`;
+    }
+    const mailOptions = {
+      from: `"KeyMed Solution" <${SmtpEmail}>`, // Sender name & email
+      to: "kishan@yopmail.com", // Recipient email
+      subject: subject,
+      html: html,
     };
-
     try {
       await transporter.sendMail(mailOptions);
-      res.status(200).json({ status: 'SUCCESS' });
+      res.status(200).json({ status: "SUCCESS" });
     } catch (error) {
-      res.status(200).json({ status: 'FAILURE', error });
+      res.status(200).json({ status: "FAILURE", error });
     }
-
   }
 }
-
-
--- src/markup/pages/contact-us.jsx
+++ src/markup/pages/contact-us.jsx
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 // Layout
 import Header from "../layout/header2";
@@ -15,147 +15,299 @@ import animateWave from "../../images/shap/wave-blue.png";
 import animate2 from "../../images/shap/circle-dots.png";
 import animateRotate from "../../images/shap/plus-blue.png";
 
+const ContactUs = () => {
+  const navigate = useNavigate();
+  const [responseMessage, setResponseMessage] = useState("");
+  const [isError, setIsError] = useState(false);
 
-class ContactUs extends Component{
-	
-	render(){
-		return (
-			<>
-				
-				<Header />
-				
-				<div className="page-content bg-white">
-					
-					<div className="banner-wraper">
-						<div className="page-banner banner-lg contact-banner" style={{backgroundImage: "url("+bnrImg1+")"}}>
-							<div className="container">
-								<div className="page-banner-entry text-center">
-									<h1>Contact Us</h1>
-									<nav aria-label="breadcrumb" className="breadcrumb-row">
-										<ul className="breadcrumb">
-											<li className="breadcrumb-item"><Link to="/"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg> Home</Link></li>
-											<li className="breadcrumb-item active" aria-current="page">Contact Us</li>
-										</ul>
-									</nav>
-								</div>
-							</div>
-							<img className="pt-img1 animate-wave" src={animateWave} alt=""/>
-							<img className="pt-img2 animate2" src={animate2} alt=""/>
-							<img className="pt-img3 animate-rotate" src={animateRotate} alt=""/>
-						</div>
-					</div>
-					
-					<section className="">
-						<div className="container">
-							<div className="contact-wraper">
-								<div className="row">
-									<div className="col-lg-6 mb-30">
-										<form className="form-wraper contact-form ajax-form">
-											<div className="ajax-message"></div>
-											<div className="row">
-												<div className="form-group col-md-12">
-													<input name="name" type="text" required className="form-control" placeholder="Your Name"/>
-												</div>
-												<div className="form-group col-md-12">
-													<input name="email" type="email" required className="form-control" placeholder="Email"/>
-												</div>
-												<div className="form-group col-md-12">
-													<input name="phone" type="text" required className="form-control" placeholder="Phone Numbers"/>
-												</div>
-												<div className="form-group col-md-12">
-													<select className="form-select form-control">
-														<option selected>Selecty Department</option>
-														<option value="1">One</option>
-														<option value="2">Two</option>
-														<option value="3">Three</option>
-													</select>
-												</div>
-												<div className="form-group col-md-12">
-													<textarea name="message" required className="form-control" placeholder="Type Message"></textarea>
-												</div>
-												<div className="col-lg-12">
-													<button name="submit" type="submit" value="Submit" className="btn w-100 btn-secondary btn-lg">Submit</button>
-												</div>
-											</div>
-										</form>
-									</div>
-									<div className="col-lg-6 mb-30">
-										<div className="contact-info ovpr-dark" style={{backgroundImage: "url("+pic1+")"}}>
-											<div className="info-inner">
-												<h4 className="title mb-30">Contact Us For Any Informations</h4>
-												<div className="icon-box">
-													<h6 className="title"><i className="ti-map-alt"></i>Location</h6>		
-													<p>Greensboro, North Carolina, USA</p>
-												</div>
-												<div className="icon-box">
-													<h6 className="title"><i className="ti-id-badge"></i>Email &amp; Phone</h6>		
-													<a href="mailto:info@keymedsolution.com" className="text-white">info@keymedsolution.com</a>
-													<br/>
-													<a href="tel:+13364999299" className="text-white">+1 (336) 499-9299</a>
-												</div>
-												<div className="icon-box">
-													<h6 className="title"><i className="ti-world"></i>Follow Us</h6>
-													<ul className="social-media">
-														<li><a rel="noreferrer" target="_blank" href="https://twitter.com/"><i className="fab fa-twitter"></i></a></li>
-														<li><a rel="noreferrer" target="_blank" href="https://www.linkedin.com/"><i className="fab fa-linkedin"></i></a></li>
-														<li><a rel="noreferrer" target="_blank" href="https://www.instagram.com/"><i className="fab fa-instagram"></i></a></li>
-													</ul>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</section>
-					
-					<section className="section-area section-sp1">
-						<div className="container">
-							<div className="row">
-								<div className="col-lg-4 col-md-6 mb-30">
-									<div className="feature-container feature-bx4 feature4">
-										<div className="icon-md feature-icon">
-											<img src={icon1} alt=""/>
-										</div>
-										<div className="icon-content">
-											<h5 className="ttr-title">Contact Number</h5><br/>
-											<p><a href="tel:+13364999299">+1 (336) 499-9299</a></p>
-										</div>
-									</div>
-								</div>
-								<div className="col-lg-4 col-md-6 mb-30">
-									<div className="feature-container feature-bx4 feature3">
-										<div className="icon-md feature-icon">
-											<img src={icon3} alt=""/>
-										</div>
-										<div className="icon-content">
-											<h5 className="ttr-title">Email Address</h5>
-											<p>info@keymedsolution.com</p>
-										</div>
-									</div>
-								</div>
-								<div className="col-lg-4 col-md-6 mb-30">
-									<div className="feature-container feature-bx4 feature2">
-										<div className="icon-md feature-icon">
-											<img src={icon2} alt=""/>
-										</div>
-										<div className="icon-content">
-											<h5 className="ttr-title">Address</h5>
-											<p>Greensboro, North Carolina, USA</p>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</section>
-				
-				</div>
-				
-				<Footer />
-				
-			</>
-		);
-	}
-}
+  async function handleOnSubmit(e) {
+    e.preventDefault();
+    setIsError(false);
+    setResponseMessage("");
+    const formData = {};
+
+    Array.from(e.currentTarget.elements).forEach((field) => {
+      if (!field.name) return;
+      formData[field.name] = field.value;
+    });
+    console.log(formData);
+
+    try {
+      const response = await fetch("/api/send-email", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ ...formData, usedFor: "contactus" }),
+      });
+
+      const resData = await response.json();
+      if (response.ok) {
+        setIsError(false);
+        setResponseMessage("Your request has been submitted successfully!");
+        navigate("/thank-you");
+      } else {
+        setIsError(true);
+        setResponseMessage("Failed to submit. Please try again.");
+      }
+    } catch (error) {
+      console.error("Error submitting form:", error);
+      setIsError(true);
+      setResponseMessage("An error occurred. Please try again later.");
+    }
+  }
+  return (
+    <>
+      <Header />
+
+      <div className="page-content bg-white">
+        <div className="banner-wraper">
+          <div
+            className="page-banner banner-lg contact-banner"
+            style={{ backgroundImage: "url(" + bnrImg1 + ")" }}
+          >
+            <div className="container">
+              <div className="page-banner-entry text-center">
+                <h1>Contact Us</h1>
+                <nav aria-label="breadcrumb" className="breadcrumb-row">
+                  <ul className="breadcrumb">
+                    <li className="breadcrumb-item">
+                      <Link to="/">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="22"
+                          height="22"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          className="feather feather-home"
+                        >
+                          <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                          <polyline points="9 22 9 12 15 12 15 22"></polyline>
+                        </svg>{" "}
+                        Home
+                      </Link>
+                    </li>
+                    <li className="breadcrumb-item active" aria-current="page">
+                      Contact Us
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+            <img className="pt-img1 animate-wave" src={animateWave} alt="" />
+            <img className="pt-img2 animate2" src={animate2} alt="" />
+            <img
+              className="pt-img3 animate-rotate"
+              src={animateRotate}
+              alt=""
+            />
+          </div>
+        </div>
+
+        <section className="">
+          <div className="container">
+            <div className="contact-wraper">
+              <div className="row">
+                <div className="col-lg-6 mb-30">
+                  <form
+                    onSubmit={handleOnSubmit}
+                    method="POST"
+                    className="form-wraper contact-form ajax-form"
+                  >
+                    <div className="ajax-message">
+                      {responseMessage && (
+                        <div
+                          className={`alert ${
+                            isError ? "alert-danger" : "alert-success"
+                          }`}
+                        >
+                          <strong> {responseMessage}</strong>
+                        </div>
+                      )}
+                    </div>
+                    <div className="row">
+                      <div className="form-group col-md-12">
+                        <input
+                          name="name"
+                          type="text"
+                          required
+                          className="form-control"
+                          placeholder="Your Name"
+                        />
+                      </div>
+                      <div className="form-group col-md-12">
+                        <input
+                          name="email"
+                          type="email"
+                          required
+                          className="form-control"
+                          placeholder="Email"
+                        />
+                      </div>
+                      <div className="form-group col-md-12">
+                        <input
+                          name="phone"
+                          type="text"
+                          required
+                          className="form-control"
+                          placeholder="Phone Numbers"
+                        />
+                      </div>
+                      <div className="form-group col-md-12">
+                        <select
+                          className="form-select form-control"
+                          name="department"
+                        >
+                          <option selected>Selecty Department</option>
+                          <option value="1">One</option>
+                          <option value="2">Two</option>
+                          <option value="3">Three</option>
+                        </select>
+                      </div>
+                      <div className="form-group col-md-12">
+                        <textarea
+                          name="message"
+                          required
+                          className="form-control"
+                          placeholder="Type Message"
+                        ></textarea>
+                      </div>
+                      <div className="col-lg-12">
+                        <button
+                          name="submit"
+                          type="submit"
+                          value="Submit"
+                          className="btn w-100 btn-secondary btn-lg"
+                        >
+                          Submit
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+                <div className="col-lg-6 mb-30">
+                  <div
+                    className="contact-info ovpr-dark"
+                    style={{ backgroundImage: "url(" + pic1 + ")" }}
+                  >
+                    <div className="info-inner">
+                      <h4 className="title mb-30">
+                        Contact Us For Any Informations
+                      </h4>
+                      <div className="icon-box">
+                        <h6 className="title">
+                          <i className="ti-map-alt"></i>Location
+                        </h6>
+                        <p>Greensboro, North Carolina, USA</p>
+                      </div>
+                      <div className="icon-box">
+                        <h6 className="title">
+                          <i className="ti-id-badge"></i>Email &amp; Phone
+                        </h6>
+                        <a
+                          href="mailto:info@keymedsolution.com"
+                          className="text-white"
+                        >
+                          info@keymedsolution.com
+                        </a>
+                        <br />
+                        <a href="tel:+13364999299" className="text-white">
+                          +1 (336) 499-9299
+                        </a>
+                      </div>
+                      <div className="icon-box">
+                        <h6 className="title">
+                          <i className="ti-world"></i>Follow Us
+                        </h6>
+                        <ul className="social-media">
+                          <li>
+                            <a
+                              rel="noreferrer"
+                              target="_blank"
+                              href="https://twitter.com/"
+                            >
+                              <i className="fab fa-twitter"></i>
+                            </a>
+                          </li>
+                          <li>
+                            <a
+                              rel="noreferrer"
+                              target="_blank"
+                              href="https://www.linkedin.com/"
+                            >
+                              <i className="fab fa-linkedin"></i>
+                            </a>
+                          </li>
+                          <li>
+                            <a
+                              rel="noreferrer"
+                              target="_blank"
+                              href="https://www.instagram.com/"
+                            >
+                              <i className="fab fa-instagram"></i>
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="section-area section-sp1">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-4 col-md-6 mb-30">
+                <div className="feature-container feature-bx4 feature4">
+                  <div className="icon-md feature-icon">
+                    <img src={icon1} alt="" />
+                  </div>
+                  <div className="icon-content">
+                    <h5 className="ttr-title">Contact Number</h5>
+                    <br />
+                    <p>
+                      <a href="tel:+13364999299">+1 (336) 499-9299</a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-4 col-md-6 mb-30">
+                <div className="feature-container feature-bx4 feature3">
+                  <div className="icon-md feature-icon">
+                    <img src={icon3} alt="" />
+                  </div>
+                  <div className="icon-content">
+                    <h5 className="ttr-title">Email Address</h5>
+                    <p>info@keymedsolution.com</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-4 col-md-6 mb-30">
+                <div className="feature-container feature-bx4 feature2">
+                  <div className="icon-md feature-icon">
+                    <img src={icon2} alt="" />
+                  </div>
+                  <div className="icon-content">
+                    <h5 className="ttr-title">Address</h5>
+                    <p>Greensboro, North Carolina, USA</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+
+      <Footer />
+    </>
+  );
+};
 
 export default ContactUs;
