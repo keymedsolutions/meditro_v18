ee3ccf1a3d33cf8e0faa80c0dd505a9797acdee7
Kishan Ramani <admin@keymedsolution.com>
2025-06-02 10:22:54 +0530

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,14 @@
 /* eslint-disable react/no-unescaped-entities */
 import NotFoundPage from '@/components/elements/NotFoundPage'
 import MainLayout from '@/components/layout/MainLayout'
+import { redirect } from 'next/navigation';
 import React from 'react'
 // Import Images
 
 
 const NotFound = () => {
+    redirect('/');
+    
     return (
         <MainLayout>
 
