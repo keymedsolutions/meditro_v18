96fbf8c9c8e911fb2bf2decf7c55f9556d7c7b43
Kishan Ramani <admin@keymedsolution.com>
2025-06-15 13:03:08 +0530

    Add new RCM plan

---
+++ src/components/Model/RCMPlanSelectModalFormModal.tsx
@@ -0,0 +1,201 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useEffect, useState } from "react";
+import { Button, Modal, Spinner } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useRouter } from "next/navigation";
+
+// Define Zod schema for form validation
+const schema = z.object({
+  name: z.string().min(1, "Name is required"),
+  email: z.string().min(1, "Email is required").email("Invalid email address"),
+  phone: z.string().min(10, "Phone number must be at least 10 digits"),
+  plan: z.string().min(1, "Plan  is required"),
+});
+
+const RCMPlanSelectModalFormModal = ({ show, handleClose, plan }: any) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, },
+    reset,
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
+
+
+  useEffect(() => {
+    if (!show) {
+      reset();
+    }
+
+    if (plan) {
+      reset({
+        plan: plan,
+      });
+    }
+  }, [show, plan]);
+  const navigate = useRouter();
+
+  const [responseMessage, setResponseMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (!show) {
+      reset();
+    }
+  }, [show]);
+
+  // Handle form submission
+  const onSubmit = async (data: any) => {
+    setResponseMessage(""); // Reset previous message
+    setIsLoading(true);
+    try {
+      const response = await fetch(
+        "/api/send-email",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ ...data, "usedFor": "rcm-plans" }),
+        }
+      );
+
+      if (response.ok) {
+        setResponseMessage("✅ Your request has been submitted successfully!");
+        reset(); // Reset form after successful submission
+        navigate.replace("/thank-you");
+      } else {
+        setResponseMessage("❌ Failed to submit. Please try again.");
+      }
+    } catch (error) {
+      setResponseMessage("❌ An error occurred. Please try again later.");
+      console.error("Error:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Modal size="lg" show={show} onHide={handleClose} centered>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Modal.Header closeButton>
+          <h3 className="tw-text-2xl tw-font-bold tw-text-accent-500">
+            Subscribe Plan
+          </h3>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="tw-rounded-xl tw-p-3 sm:tw-p-4 md:tw-p-8">
+            {/* Name Field */}
+            <div className="tw-mb-4">
+              <label className="tw-block tw-text-accentOrange-500 tw-font-bold tw-mb-2">
+                Name
+              </label>
+              <input
+                type="text"
+                {...register("name")}
+                className="tw-w-full tw-px-3 tw-py-2 tw-border tw-rounded-lg"
+              />
+              {errors.name && (
+                <p className="tw-text-red-500 tw-text-sm">
+                  {errors.name.message}
+                </p>
+              )}
+            </div>
+
+            {/* Email Field */}
+            <div className="tw-mb-4">
+              <label className="tw-block tw-text-accentOrange-500 tw-font-bold tw-mb-2">
+                Email
+              </label>
+              <input
+                type="email"
+                {...register("email")}
+                className="tw-w-full tw-px-3 tw-py-2 tw-border tw-rounded-lg"
+              />
+              {errors.email && (
+                <p className="tw-text-red-500 tw-text-sm">
+                  {errors.email.message}
+                </p>
+              )}
+            </div>
+
+            {/* Phone Field */}
+            <div className="tw-mb-4">
+              <label className="tw-block tw-text-accentOrange-500 tw-font-bold tw-mb-2">
+                Phone Number
+              </label>
+              <input
+                type="tel"
+                {...register("phone")}
+                className="tw-w-full tw-px-3 tw-py-2 tw-border tw-rounded-lg"
+              />
+              {errors.phone && (
+                <p className="tw-text-red-500 tw-text-sm">
+                  {errors.phone.message}
+                </p>
+              )}
+            </div>
+
+            {/* Service Type (Dropdown) */}
+            <div className="tw-mb-4">
+              <label
+                htmlFor="practice"
+                className="tw-block tw-text-accentOrange-500 tw-font-bold tw-mb-2"
+              >
+                Plan
+              </label>
+              <select
+                id="practice"
+                {...register("plan")}
+                className="tw-w-full tw-px-3 tw-py-2 tw-border tw-border-gray-300 tw-bg-transparent tw-rounded-lg focus:tw-outline-none focus:tw-ring-2 focus:tw-ring-accentOrange-500"
+              >
+                <option value="">Select a Plan</option>
+
+                <option value="$8-Complete-Insurance-Breakdown">Complete Insurance Breakdown</option>
+                <option value="$2-Eligibility-Only-Verification">Eligibility-Only Verification</option>
+                <option value="Subscription-Dedicated-Verification-Agents">Dedicated Verification Agents</option>
+              </select>
+              {errors.plan && (
+                <p className="tw-text-red-500 tw-text-sm tw-mt-1">
+                  {errors.plan.message}
+                </p>
+              )}
+            </div>
+          </div>
+
+          {responseMessage && (
+            <p
+              className={`tw-text-center tw-mt-4 ${responseMessage.includes("✅")
+                ? "tw-text-green-500"
+                : "tw-text-red-500"
+                }`}
+            >
+              {responseMessage}
+            </p>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="secondary"
+            onClick={handleClose}
+            disabled={isLoading}
+          >
+            Close
+          </Button>
+          <Button variant="primary" type="submit" disabled={isLoading}>
+            {isLoading ? (
+              <Spinner as="span" animation="border" size="sm" />
+            ) : (
+              "Subscribe Now"
+            )}
+          </Button>
+        </Modal.Footer>
+      </form>
+    </Modal>
+  );
+};
+
+export default RCMPlanSelectModalFormModal;
--- src/components/layout/BrandRattings.tsx
+++ src/components/layout/BrandRattings.tsx
@@ -1,76 +1,109 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client';
+
+import { Star } from 'lucide-react';
+
+const achievements = [
+  {
+    name: 'GoodFirms',
+    logo: '/images/logo/1.png',
+    rating: null,
+    description: 'Top Medical Biling Company ',
+    link: 'https://www.goodfirms.co/company/key-medsolutions',
+  },
+  {
+    name: 'Crunchbase',
+    logo: '/images/logo/2.png',
+    rating: null,
+    description: 'Top Medical Biling Company ',
+    link: 'https://www.crunchbase.com/organization/key-medsolutions',
+  },
+  // {
+  //   name: 'Trustpilot Widget',
+  //   isWidget: true, // for Trustpilot widget
+  // },
+  {
+    name: 'Clutch',
+    logo: '/images/logo/3.png',
+    rating: null,
+    description: 'Top Medical Biling Company',
+    link: 'https://clutch.co/profile/key-medsolutions',
+  },
+  {
+    name: 'TrueFirms',
+    logo: '/images/logo/4.png',
+    rating: null,
+    description: 'Top mobile developers',
+    link: 'https://www.truefirms.co/company/Key-MedSolutions/3265',
+  },
+  {
+    name: 'BusinessFirms',
+    logo: '/images/logo/5.png',
+    rating: null,
+    description: 'Top Medical Biling Company ',
+    link: 'https://businessfirms.co/company/key-medsolutions',
+  },
+  {
+    name: 'Trustpilot',
+    logo: '/images/logo/6.png',
+    rating: null,
+    description: 'Top Medical Biling Company',
+    link: 'https://www.trustpilot.com/review/keymedsolution.com',
+  },
+];
 
 const BrandRatings = () => {
   return (
-    <div className="tw-grid tw-grid-cols-3 md:tw-grid-cols-6 tw-gap-6 tw-p-6 tw-bg-white">
-      <a
-        href="https://www.goodfirms.co/company/key-medsolutions"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="tw-bg-white tw-border tw-border-purple-200 tw-rounded-xl tw-p-4 tw-flex tw-justify-center tw-items-center tw-shadow-sm tw-transition-transform hover:tw-scale-105"
-      >
-        <img src="/images/logo/1.png" alt="GoodFirms" className="tw-w-16 tw-h-16 tw-object-contain" />
-      </a>
-
-      <a
-        href="https://www.crunchbase.com/organization/key-medsolutions"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="tw-bg-white tw-border tw-border-purple-200 tw-rounded-xl tw-p-4 tw-flex tw-justify-center tw-items-center tw-shadow-sm tw-transition-transform hover:tw-scale-105"
-      >
-        <img src="/images/logo/2.png" alt="Crunchbase" className="tw-w-16 tw-h-16 tw-object-contain" />
-      </a>
-      {/* Trustpilot */}
-      <div className="tw-bg-white tw-rounded-xl tw-shadow-md tw-p-4">
-        <div
-          className="trustpilot-widget"
-          data-locale="en-US"
-          data-template-id="56278e9abfbbba0bdcd568bc"
-          data-businessunit-id="67dd01024e5a60fbbd3ce1c6"
-          data-style-height="150px"
-          data-style-width=""
-          data-theme="light"
-        >
-          <a href="https://www.trustpilot.com/review/keymedsolution.com" target="_blank" rel="noopener noreferrer">Trustpilot</a>
-        </div>
+    <section className="tw-py-12 tw-bg-gray-50">
+      <div className="tw-text-center tw-mb-10">
+        <h2 className="tw-text-2xl tw-font-bold tw-text-gray-800">Our Achievements & Certifications</h2>
       </div>
-
-      <a
-        href="https://clutch.co/profile/key-medsolutions"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="tw-bg-white tw-border tw-border-purple-200 tw-rounded-xl tw-p-4 tw-flex tw-justify-center tw-items-center tw-shadow-sm tw-transition-transform hover:tw-scale-105"
-      >
-        <img src="/images/logo/3.png" alt="Clutch" className="tw-w-16 tw-h-16 tw-object-contain" />
-      </a>
-
-      <a
-        href="https://www.truefirms.co/company/Key-MedSolutions/3265"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="tw-bg-white tw-border tw-border-purple-200 tw-rounded-xl tw-p-4 tw-flex tw-justify-center tw-items-center tw-shadow-sm tw-transition-transform hover:tw-scale-105"
-      >
-        <img src="/images/logo/4.png" alt="TrueFirms" className="tw-w-16 tw-h-16 tw-object-contain" />
-      </a>
-
-      <a
-        href="https://businessfirms.co/company/key-medsolutions"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="tw-bg-white tw-border tw-border-purple-200 tw-rounded-xl tw-p-4 tw-flex tw-justify-center tw-items-center tw-shadow-sm tw-transition-transform hover:tw-scale-105"
-      >
-        <img src="/images/logo/5.png" alt="BusinessFirms" className="tw-w-16 tw-h-16 tw-object-contain" />
-      </a>
-
-      <a
-        href="https://www.trustpilot.com/review/keymedsolution.com"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="tw-bg-white tw-border tw-border-purple-200 tw-rounded-xl tw-p-4 tw-flex tw-justify-center tw-items-center tw-shadow-sm tw-transition-transform hover:tw-scale-105"
-      >
-        <img src="/images/logo/6.png" alt="Trustpilot" className="tw-w-16 tw-h-16 tw-object-contain" />
-      </a>
-    </div>
+      <div className="tw-grid tw-grid-cols-1 sm:tw-grid-cols-2 md:tw-grid-cols-4 tw-gap-6 tw-max-w-6xl tw-mx-auto">
+        {achievements.map((item:any, index) => (
+          <div
+            key={index}
+            className="tw-bg-white tw-rounded-xl tw-p-6 tw-shadow-md tw-border tw-border-gray-200 tw-flex tw-flex-col tw-items-center tw-text-center tw-space-y-3 hover:tw-shadow-lg tw-transition"
+          >
+            {item?.isWidget ? (
+              // Trustpilot widget
+              <div className="trustpilot-widget"
+                data-locale="en-US"
+                data-template-id="56278e9abfbbba0bdcd568bc"
+                data-businessunit-id="67dd01024e5a60fbbd3ce1c6"
+                data-style-height="150px"
+                data-style-width=""
+                data-theme="light"
+              >
+                <a href="https://www.trustpilot.com/review/keymedsolution.com" target="_blank" rel="noopener noreferrer">
+                  Trustpilot Widget
+                </a>
+              </div>
+            ) : (
+              <>
+                <a href={item.link} target="_blank" rel="noopener noreferrer">
+                  <img src={item.logo} alt={item.name} className="tw-w-full tw-h-24  tw-mx-auto" />
+                </a>
+                {item.rating && (
+                  <div className="tw-flex tw-items-center tw-justify-center tw-gap-1">
+                    {Array.from({ length: 5 }).map((_, i) => (
+                      <Star
+                        key={i}
+                        className={`tw-w-4 tw-h-4 ${
+                          i < Math.floor(item.rating) ? 'tw-text-yellow-400' : 'tw-text-gray-300'
+                        }`}
+                        fill={i < Math.floor(item.rating) ? 'currentColor' : 'none'}
+                      />
+                    ))}
+                    <span className="tw-font-semibold tw-text-gray-700">{item.rating}</span>
+                  </div>
+                )}
+                <p className="tw-text-sm tw-text-gray-500">{item.description}</p>
+              </>
+            )}
+          </div>
+        ))}
+      </div>
+    </section>
   );
 };
 
