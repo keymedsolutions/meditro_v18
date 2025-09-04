51225d4385492b35c903a0e86e562b7ecb49f4ea
keymedsolutions <132235902+keymedsolutions@users.noreply.github.com>
2025-09-04 07:37:53 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,9 +11,9 @@ export function middleware(request: NextRequest) {
   headers.set("x-full-path", request.nextUrl.href);
 
   // console.log(!isRouteEnabled(pathname),"pppppppp")
-  if (!isRouteEnabled(pathname)) {
-    return NextResponse.redirect(new URL("/maintenance", request.url));
-  }
+  //if (!isRouteEnabled(pathname)) {
+    //return NextResponse.redirect(new URL("/maintenance", request.url));
+ // }
 
   return NextResponse.next({ headers });
 }
@@ -21,3 +21,4 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
+
